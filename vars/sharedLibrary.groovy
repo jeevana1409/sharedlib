c2622fbab3a2f9c4ecac0b2bc8be91e07d036112
jeevana1409 <gandejeevanasai@gmail.com>
2026-02-19 08:55:17 +0530

    Update sharedLibrary.groovy

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -2,34 +2,3 @@ def gitDownload(repo)
 {
   git "https://github.com/jeevana1409/${repo}.git"
 }
-
-def build()
-{ 
-  sh 'mvn package'
-}
-
-def dockercontext()
-{
-  sh '''
-     mkdir -p docker
-        cp webapp/target/webapp.war docker/webapp.war
-        cat <<EOF > docker/Dockerfile
-FROM tomcat:10
-COPY webapp.war /usr/local/tomcat/webapps/
-EXPOSE 8080
-EOF
-   '''
-}
-
-def dockerBuild(imageName)
-{
-  sh "docker build -t ${imageName} docker"
-}
-
-def deployment(containerName, imageName)
-{
-  sh '''
-       docker rm -f ${containerName} || true
-        docker run -d -p 8081:8080 --name ${containerName} ${imageName}
-    '''
-}
