7bd9e115dd7cb4b5852b03c341b44f6b51d05d69
jeevana1409 <gandejeevanasai@gmail.com>
2026-02-19 07:41:36 +0530

    Create sharedlibrary.groovy

---
+++ vars/sharedlibrary.groovy
@@ -0,0 +1,35 @@
+def gitdownload (repo)
+{
+  git "https://github.com/jeevana1409/${repo}.git"
+}
+
+def build ()
+{ 
+  sh 'mvn package'
+}
+
+def dockercontex()
+{
+  sh '''
+     mkdir -p docker
+        cp webapp/target/webapp.war docker/webapp.war
+        cat <<EOF > docker/Dockerfile
+FROM tomcat:10
+COPY webapp.war /usr/local/tomcat/webapps/
+EXPOSE 8080
+EOF
+   '''
+}
+
+def docker build(imageName)
+{
+  sh "docker build -t $(imageName) docker"
+}
+
+def deployment(containerName, imageName)
+{
+  sh '''
+       docker rm -f ${containerName} || true
+        docker run -d -p 8081:8080 --name ${containerName} ${imageName}
+    '''
+}
